e0f860a445e786b4ce826b2abac2c7a5d9747d64
trd-admin <trd-admin@trd-admin.routeree98b0.com>
2019-09-26 15:06:27 +0800

    This is the database and to be exported in PMA

---
+++ mp6_db.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Sep 26, 2019 at 09:04 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mp6_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `prof_id` int(11) NOT NULL,
+  `prof_name` varchar(50) NOT NULL,
+  `prof_apartment` varchar(50) NOT NULL,
+  `prof_landlord` varchar(50) NOT NULL,
+  `prof_address` varchar(100) NOT NULL,
+  `prof_talents` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`prof_id`, `prof_name`, `prof_apartment`, `prof_landlord`, `prof_address`, `prof_talents`) VALUES
+(1, 'Shammah Casumpang', 'Bahay ni Shammah', 'Malaking Kapatid', 'Bangkas A', 'Dancing, Singing, Coding'),
+(2, 'Shammae Supsup', 'Apartmenthol', 'Mr. Crab', 'Bangkas B', 'Dancing, Reading, Coding, Acting, Praying');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`prof_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `prof_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
